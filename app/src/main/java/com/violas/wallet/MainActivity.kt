a749429cae6f60f8a57423fedef3571af3df2ca7
CharlesXiang <xsx@palliums.org>
2019-10-21 10:30:07 +0800

    1.fix build error； 2.统一依赖库版本。

--- app/src/main/java/com/violas/wallet/MainActivity.kt
+++ app/src/main/java/com/violas/wallet/MainActivity.kt
@@ -1,16 +1,12 @@
 package com.violas.wallet
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.smallraw.core.keystoreCompat.KeystoreCompat
-import com.violas.wallet.repository.DataRepository
-import org.junit.Assert
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)ß
+        setContentView(R.layout.activity_main)
     }
 }
