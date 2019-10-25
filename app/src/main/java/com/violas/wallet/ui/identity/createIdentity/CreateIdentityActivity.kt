be5294caa2ec9fa20d4ca6d6196ebc72efdd4ba9
QuincySx <QuincySx@sina.com>
2019-10-25 18:09:27 +0800

    modify 创建账户 UI

--- app/src/main/java/com/violas/wallet/ui/identity/createIdentity/CreateIdentityActivity.kt
+++ app/src/main/java/com/violas/wallet/ui/identity/createIdentity/CreateIdentityActivity.kt
@@ -24,11 +24,26 @@ class CreateIdentityActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        title = getString(R.string.title_import_the_wallet)
-
+        title = getString(R.string.title_create_the_wallet)
+        setTitleStyle(TITLE_STYLE_MAIN_COLOR)
         btnConfirm.setOnClickListener {
             val walletName = editName.text.toString().trim()
             val password = editPassword.text.toString().trim().toByteArray()
+            val passwordConfirm = editConfirmPassword.text.toString().trim().toByteArray()
+
+            if (walletName.isEmpty()) {
+                showToast(getString(R.string.hint_nickname_empty))
+                return@setOnClickListener
+            }
+            if (editPassword.text.toString().length < 6) {
+                showToast(getString(R.string.hint_input_password_short))
+                return@setOnClickListener
+            }
+            if (!password.contentEquals(passwordConfirm)) {
+                showToast(getString(R.string.hint_confirm_password_fault))
+                return@setOnClickListener
+            }
+
             showProgress()
             launch(Dispatchers.IO) {
                 AccountManager().createIdentity(
