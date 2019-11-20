754324444fce269a4d3fe79b492fa6fec8bf4590
QuincySx <QuincySx@sina.com>
2019-11-20 16:41:32 +0800

    violas:修改合约版本

--- lib-violas-core/src/androidTest/java/org/palliums/violascore/move/MoveHandlerTest.kt
+++ lib-violas-core/src/androidTest/java/org/palliums/violascore/move/MoveHandlerTest.kt
@@ -5,7 +5,9 @@ import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.palliums.violascore.move.Move.violasTokenEncode
 import org.palliums.violascore.serialization.hexToBytes
+import org.palliums.violascore.serialization.toHex
 
 @RunWith(AndroidJUnit4::class)
 class MoveHandlerTest {
@@ -15,6 +17,16 @@ class MoveHandlerTest {
         val moveCode = Move.decode(appContext.assets.open("move/token_transfer.json"))
         val tokenAddress = "7257c2417e4d1038e1817c8f283ace2e1041b3396cdbb099eb357bbee024d614".hexToBytes()
         val index = Move.findAddressIndex(moveCode,tokenAddress)
-        Assert.assertEquals(index,161)
+        Assert.assertEquals(index,156)
+    }
+
+    @Test
+    fun test_new_move_code(){
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        val violasTokenEncode = violasTokenEncode(
+            appContext.assets.open("move/token_transfer.json"),
+            "05599ef248e215849cc599f563b4883fc8aff31f1e43dff1e3ebe4de1370e054".hexToBytes()
+        )
+        Assert.assertEquals("4c49425241564d0a010007014a00000004000000034e000000060000000d54000000060000000e5a0000000600000005600000001c000000047c0000004000000008bc0000000f00000000000101000200010300020002040200030204020300063c53454c463e0644546f6b656e046d61696e087472616e73666572000000000000000000000000000000000000000000000000000000000000000005599ef248e215849cc599f563b4883fc8aff31f1e43dff1e3ebe4de1370e054000100020004000c000c0113010102",violasTokenEncode.toHex())
     }
 }
