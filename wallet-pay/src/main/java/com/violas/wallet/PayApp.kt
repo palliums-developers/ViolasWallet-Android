6926360946cfafc0ae6121475cd50bfd040e2bac
QuincySx <QuincySx@sina.com>
2019-12-30 17:06:41 +0800

    接入 bugly 应用更新

--- wallet-pay/src/main/java/com/violas/wallet/PayApp.kt
+++ wallet-pay/src/main/java/com/violas/wallet/PayApp.kt
@@ -2,13 +2,13 @@ package com.violas.wallet
 
 import android.content.Context
 import com.palliums.content.App
-import com.tencent.bugly.crashreport.CrashReport
+import com.tencent.bugly.Bugly
 import com.violas.wallet.ui.changeLanguage.MultiLanguageUtility
 
 class PayApp : App() {
     override fun onCreate() {
         super.onCreate()
-        CrashReport.initCrashReport(applicationContext, "f4d9c546fa", false);
+        Bugly.init(applicationContext, "f4d9c546fa", false);
     }
 
     override fun attachBaseContext(newBase: Context) {
--- wallet-sso/src/main/java/com/violas/wallet/SSOApp.kt
+++ wallet-sso/src/main/java/com/violas/wallet/SSOApp.kt
@@ -2,13 +2,13 @@ package com.violas.wallet
 
 import android.content.Context
 import com.palliums.content.App
-import com.tencent.bugly.crashreport.CrashReport
+import com.tencent.bugly.Bugly
 import com.violas.wallet.ui.changeLanguage.MultiLanguageUtility
 
 class SSOApp : App() {
     override fun onCreate() {
         super.onCreate()
-        CrashReport.initCrashReport(applicationContext, "beaf9fe271", false);
+        Bugly.init(applicationContext, "beaf9fe271", false);
     }
 
     override fun attachBaseContext(newBase: Context) {
