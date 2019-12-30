d98a1a381adc7629b2c3e07a789d4a8a6fd4aa56
QuincySx <QuincySx@sina.com>
2019-12-30 16:18:20 +0800

    添加 BUG 上报平台

--- wallet-pay/src/main/java/com/violas/wallet/PayApp.kt
+++ wallet-pay/src/main/java/com/violas/wallet/PayApp.kt
@@ -2,9 +2,14 @@ package com.violas.wallet
 
 import android.content.Context
 import com.palliums.content.App
+import com.tencent.bugly.crashreport.CrashReport
 import com.violas.wallet.ui.changeLanguage.MultiLanguageUtility
 
 class PayApp : App() {
+    override fun onCreate() {
+        super.onCreate()
+        CrashReport.initCrashReport(applicationContext, "f4d9c546fa", false);
+    }
 
     override fun attachBaseContext(newBase: Context) {
         super.attachBaseContext(newBase)
