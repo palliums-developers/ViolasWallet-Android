e7ef4ce38068bc73933a9927a259d51e0eb3a115
CharlesXiang <xsx@palliums.org>
2020-03-02 10:37:57 +0800

    lib-common: 增加通用的页面loading视图

--- lib-common/src/main/java/com/palliums/widget/status/IStatusLayout.kt
+++ lib-common/src/main/java/com/palliums/widget/status/IStatusLayout.kt
@@ -1,6 +1,7 @@
 package com.palliums.widget.status
 
 import android.graphics.drawable.Drawable
+import android.widget.TextView
 import androidx.annotation.DrawableRes
 import androidx.annotation.IntDef
 import androidx.annotation.StringRes
@@ -15,6 +16,7 @@ interface IStatusLayout {
 
     @IntDef(
         Status.STATUS_NONE,
+        Status.STATUS_LOADING,
         Status.STATUS_EMPTY,
         Status.STATUS_FAILURE,
         Status.STATUS_NO_NETWORK
@@ -22,9 +24,10 @@ interface IStatusLayout {
     annotation class Status {
         companion object {
             const val STATUS_NONE = 1           // 默认状态，隐藏
-            const val STATUS_EMPTY = 2          // 空状态
-            const val STATUS_FAILURE = 3        // 失败状态
-            const val STATUS_NO_NETWORK = 4     // 无网络状态
+            const val STATUS_LOADING = 2        // 加载状态
+            const val STATUS_EMPTY = 3          // 空状态
+            const val STATUS_FAILURE = 4        // 失败状态
+            const val STATUS_NO_NETWORK = 5     // 无网络状态
         }
     }
 
@@ -33,32 +36,77 @@ interface IStatusLayout {
      * @param status
      * @param imageRes
      */
-    fun setImageWithStatus(@Status status: Int, @DrawableRes imageRes: Int)
+    fun setImageWithStatus(@Status status: Int, @DrawableRes imageRes: Int): IStatusLayout
 
     /**
      * 设置不同状态下的图片
      * @param status
      * @param image
      */
-    fun setImageWithStatus(@Status status: Int, image: Drawable)
+    fun setImageWithStatus(@Status status: Int, image: Drawable): IStatusLayout
 
     /**
      * 设置不同状态下的提示文本
      * @param status
      * @param tipsRes
      */
-    fun setTipsWithStatus(@Status status: Int, @StringRes tipsRes: Int)
+    fun setTipsWithStatus(@Status status: Int, @StringRes tipsRes: Int): IStatusLayout
 
     /**
      * 设置不同状态下的提示文本
      * @param status
      * @param tips
      */
-    fun setTipsWithStatus(@Status status: Int, tips: String)
+    fun setTipsWithStatus(@Status status: Int, tips: String): IStatusLayout
 
     /**
      * 显示状态
      * @param status
      */
     fun showStatus(@Status status: Int, errorMsg: String? = null)
+
+    /**
+     * 设置提示文本的字体大小
+     */
+    fun setTipsTextSize(size: Int): IStatusLayout
+
+    /**
+     * 设置提示文本的字体颜色
+     */
+    fun setTipsTextColor(color: Int): IStatusLayout
+
+    /**
+     * 设置重试文本
+     */
+    fun setReloadText(@StringRes resId: Int): IStatusLayout
+
+    /**
+     * 设置重试文本
+     */
+    fun setReloadText(text: String): IStatusLayout
+
+    /**
+     * 设置重试文本的字体大小
+     */
+    fun setReloadTextSize(size: Int): IStatusLayout
+
+    /**
+     * 设置重试文本的字体颜色
+     */
+    fun setReloadTextColor(color: Int): IStatusLayout
+
+    /**
+     * 获取重试控件
+     */
+    fun getReloadTextView(): TextView
+
+    /**
+     * 设置失败时显示重试
+     */
+    fun setShowReloadOnFailed(show: Boolean): IStatusLayout
+
+    /**
+     * 设置重试回调
+     */
+    fun setReloadCallback(callback: () -> Unit): IStatusLayout
 }
