b15711371fa50e5f5358ed7643e72817a6504fdb
QuincySx <QuincySx@sina.com>
2020-03-06 09:44:49 +0800

    适配 3.4 号 Libra 升级

--- lib-libra-core/src/main/java/org/palliums/libracore/transaction/RawTransactionBuilder.kt
+++ lib-libra-core/src/main/java/org/palliums/libracore/transaction/RawTransactionBuilder.kt
@@ -1,7 +1,7 @@
 package org.palliums.libracore.transaction
 
 import android.content.Context
-import org.palliums.libracore.move.Move
+import org.palliums.libracore.serialization.hexToBytes
 import org.palliums.libracore.utils.HexUtils
 import java.util.*
 
@@ -36,7 +36,9 @@ fun TransactionPayload.Companion.optionTransactionPayload(
     address: String,
     amount: Long
 ): TransactionPayload {
-    val moveEncode = Move.decode(context.assets.open("move/libra_peer_to_peer_transfer.json"))
+//    val moveEncode = Move.decode(context.assets.open("move/libra_peer_to_peer_transfer.json"))
+    val moveEncode =
+        "a11ceb0b010007014600000004000000034a000000060000000c50000000060000000d5600000006000000055c0000002900000004850000002000000007a50000000f00000000000002000100010300020002050300030205030300063c53454c463e046d61696e0c4c696272614163636f756e740f7061795f66726f6d5f73656e6465720000000000000000000000000000000000000000000000000000000000000000000000020004000b000b0112010102".hexToBytes()
 
     val addressArgument = TransactionArgument.newAddress(address)
     val amountArgument = TransactionArgument.newU64(amount)
@@ -60,9 +62,11 @@ fun TransactionPayload.Companion.optionWithDataPayload(
     val amountArgument = TransactionArgument.newU64(exchangeSendAmount)
     val dateArgument = TransactionArgument.newByteArray(data)
 
-    val moveEncode = Move.decode(
-        context.assets.open("move/libra_transfer_with_data.json")
-    )
+//    val moveEncode = Move.decode(
+//        context.assets.open("move/libra_transfer_with_data.json")
+//    )
+    val moveEncode =
+        "a11ceb0b010007014600000004000000034a000000060000000c50000000080000000d580000000800000005600000003700000004970000002000000007b7000000110000000000000200010001030002000305030b0200030305030b020300063c53454c463e046d61696e0c4c696272614163636f756e741d7061795f66726f6d5f73656e6465725f776974685f6d657461646174610000000000000000000000000000000000000000000000000000000000000000000000030005000b000b010b0212010102".hexToBytes()
 
     return TransactionPayload(
         TransactionPayload.Script(
